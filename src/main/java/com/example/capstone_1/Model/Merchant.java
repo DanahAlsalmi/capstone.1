45f8f42d1f5d2151428673bd6d6d1a1ef1dc5ccc
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-08-07 02:33:40 +0300

    Add files via upload

---
+++ src/main/java/com/example/capstone_1/Model/Merchant.java
@@ -0,0 +1,21 @@
+package com.example.capstone_1.Model;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Merchant {
+
+    @NotNull(message = "ID must not be empty")
+    private int id;
+
+    @NotEmpty(message = "Name must not be empty")
+    @Size(min = 3, message = "Name must be more than 3 characters")
+    private String name;
+
+}
